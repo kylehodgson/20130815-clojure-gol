0ef52a0ade6b9a039bd7d748d86e3f04028faa2c
Kyle Hodgson <kyle.hodgson@gmail.com>
2013-08-18 15:27:57 -0400

    working on edge detection

--- gol/src/gol/core.clj
+++ gol/src/gol/core.clj
@@ -14,16 +14,22 @@
 
 (defn extract-neighborhood [board x y]
 	(def size (count board))
+	(def top-row (dec y))
+	(def middle-row y)
+	(def bottom-row (inc y))
+	(def left-col (dec x))
+	(def middle-col x)
+	(def right-col (inc x))
 	(concat 
-		[(nth (board (- y 1) ) (- x 1) 0)
-	     (nth (board (- y 1) )    x    0)
-	     (nth (board (- y 1) ) (+ x 1) 0)]
-	    [(nth (board    y    ) (- x 1) 0)
-	     (nth (board    y    )    x    0)
-	     (nth (board    y    ) (+ x 1) 0)]
-	    [(nth (board (+ y 1) ) (- x 1) 0)
-	     (nth (board (+ y 1) )    x    0)
-	     (nth (board (+ y 1) ) (+ x 1) 0)] ))
+		[(nth (board top-row   ) left-col   0)
+	     (nth (board top-row   ) middle-col 0)
+	     (nth (board top-row   ) right-col  0)]
+	    [(nth (board middle-row) left-col   0)
+	     (nth (board middle-row) middle-col 0)
+	     (nth (board middle-row) right-col  0)]
+	    [(nth (board bottom-row) left-col   0)
+	     (nth (board bottom-row) middle-col 0)
+	     (nth (board bottom-row) right-col  0)] ))
 
 (defn live-neighbors [board x y]
 	(count-live-neighbors
